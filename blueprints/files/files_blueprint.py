05bfbd2490b7bd51e93f4b0adfad77a15e1de232
Arpit Verma <arpitmanit.01@gmail.com>
2024-05-18 10:51:56 +0530

    Added Exception Handling

--- blueprints/files/files_blueprint.py
+++ blueprints/files/files_blueprint.py
@@ -17,45 +17,56 @@ upload_parser.add_argument('file', type=werkzeug.datastructures.FileStorage, loc
 class UploadFile(Resource):
     @files_namespace.expect(upload_parser)
     def post(self):
-        args = upload_parser.parse_args()
+        result_msg = None
+        try:
+            args = upload_parser.parse_args()
 
-        # Generate a unique ID for the file
-        file_id = str(uuid.uuid4())
+            # Generate a unique ID for the file
+            file_id = str(uuid.uuid4())
 
-        # Global dictionary to store file metadata
-        file_metadata = {}
+            # Global dictionary to store file metadata
+            file_metadata = {}
 
-        # Store the file and its metadata
-        file_metadata[file_id] = {
-            'filename': args['file'].filename,
-            'content_type': args['file'].mimetype,
-            'size': args['file'].content_length
-        }
+            # Store the file and its metadata
+            file_metadata[file_id] = {
+                'filename': args['file'].filename,
+                'content_type': args['file'].mimetype,
+                'size': args['file'].content_length
+            }
 
-        # Check if file_metadata contains the required keys
-        if 'filename' not in file_metadata[file_id] or 'content_type' not in file_metadata[file_id] or 'size' not in \
-                file_metadata[file_id]:
-            return {"error": "Failed to extract file metadata"}, 500
+            # Check if file_metadata contains the required keys
+            if 'filename' not in file_metadata[file_id] or 'content_type' not in file_metadata[file_id] or 'size' not in \
+                    file_metadata[file_id]:
+                return {"error": "Failed to extract file metadata"}, 500
 
-        print(file_metadata[file_id])
-        # Save the file content to the database
-        storage_handler.add_file(
-            file_id,
-            file_metadata[file_id]['filename'],
-            file_metadata[file_id]['content_type'],
-            file_metadata[file_id]['size'],
-            args['file'].read()
-        )
-
-        return {'message': 'File uploaded successfully', 'id': file_id}
+            print(file_metadata[file_id])
+            # Save the file content to the database
+            storage_handler.add_file(
+                file_id,
+                file_metadata[file_id]['filename'],
+                file_metadata[file_id]['content_type'],
+                file_metadata[file_id]['size'],
+                args['file'].read()
+            )
+            return {'message': 'File uploaded successfully', 'id': file_id}, 200
+        except Exception as e:
+            result_msg = {
+                'message': str(e)
+            }
+            return result_msg
 
 
 @files_namespace.route('/files', methods=["GET"])
 class GetAllFiles(Resource):
     def get(self):
-        result = storage_handler.get_all()
-
-        return result
+        try:
+            result = storage_handler.get_all()
+            return result, 200
+        except Exception as e:
+            result_msg = {
+                'message': str(e)
+            }
+            return result_msg
 
 
 getid_parser = reqparse.RequestParser()
@@ -67,10 +78,16 @@ getid_parser.add_argument('id', type=str, help="Unique ID generated for that fil
 class GetIdFile(Resource):
     @files_namespace.expect(getid_parser)
     def get(self):
-        args = upload_parser.parse_args()
-        content_id = args.get('id')
-        result = storage_handler.get_by_id(content_id)
-        return result
+        try:
+            args = upload_parser.parse_args()
+            content_id = args.get('id')
+            result = storage_handler.get_by_id(content_id)
+            return result
+        except Exception as e:
+            result_msg = {
+                'message': str(e)
+            }
+            return result_msg
 
 
 rename_parser = reqparse.RequestParser()
@@ -83,12 +100,20 @@ rename_parser.add_argument('new_name', type=str, help="New Name for file",
 @files_namespace.route('/update/id', methods=["PUT"])
 class UpdateIdFile(Resource):
     @files_namespace.expect(rename_parser)
-    def get(self):
-        args = upload_parser.parse_args()
-        content_id = args.get('id')
-        new_name = args.get('new_name')
-        result = storage_handler.update_by_id(content_id, new_name)
-        return None
+    def put(self):
+        try:
+            args = upload_parser.parse_args()
+            content_id = args.get('id')
+            new_name = args.get('new_name')
+            result = storage_handler.update_by_id(content_id, new_name)
+            result_msg = {
+                'message': "Update Success"
+            }, 200
+        except Exception as e:
+            result_msg = {
+                'message': str(e)
+            }
+            return result_msg
 
 
 deleteid_parser = reqparse.RequestParser()
@@ -96,11 +121,19 @@ deleteid_parser.add_argument('id', type=str, help="Unique ID generated for that 
                              trim=True, required=True, nullable=False)
 
 
-@files_namespace.route('/file/id', methods=["GET"])
+@files_namespace.route('/file/id', methods=["delete"])
 class GetIdFile(Resource):
     @files_namespace.expect(deleteid_parser)
-    def get(self):
-        args = upload_parser.parse_args()
-        content_id = args.get('id')
-        result = storage_handler.delete_by_id(content_id)
-        return None
+    def delete(self):
+        try:
+            args = upload_parser.parse_args()
+            content_id = args.get('id')
+            result = storage_handler.delete_by_id(content_id)
+            result_msg = {
+                'message': "Delete Success"
+            }, 200
+        except Exception as e:
+            result_msg = {
+                'message': str(e)
+            }
+            return result_msg
